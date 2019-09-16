c3084c0cdbe030417f56caee7f1e5cd7f251a9d3
Amandaptr11 <55323776+Amandaptr11@users.noreply.github.com>
2019-09-16 21:07:34 +0700

    Add files via upload

---
+++ Tugas1.php
@@ -0,0 +1,56 @@
+<h1> Luas Segitiga</h1> 
+<?php
+//VisualCode Amanda Putri Rahayu
+//Luas Segitiga
+$a = 15;
+$t = 24;
+echo "panjang alas = $a";
+print "<br>";
+echo "tinggi = $t";
+print "<br>";
+echo "Rumus luas segitiga adalah";
+echo "<br>";
+echo "1/2 x alas x tinggi";
+echo "<br>";
+echo "Luas segitiga adalah ";
+echo '<br>';
+echo "1/2 x $a x $t=";
+echo $a *  $t / 2 ;
+?>
+
+<h1> Luas Jajar Genjang</h1>
+<?php
+//Luas Jajar genjang
+$x = 18;
+$y = 20;
+echo "alas = $x";
+echo "<br>";
+echo "tinggi = $y";
+echo "<br>";
+echo "Rumus luas jajar genjang adalah";
+echo "<br>";
+echo "alas x tinggi";
+echo "<br>";
+echo "Luas jajar genjang adalah";
+echo "<br>";
+echo "$x x $y =";
+echo $x * $y;
+?>
+
+<h1> Luas Lingkaran </h1>
+<?php
+$phi = 22 / 7;
+$r = 35;
+echo "π = $phi";
+echo "<br>";
+echo "r = $r";
+echo "<br>";
+echo "Rumus luas lingkaran adalah";
+echo "<br>";
+echo "π × r²";
+echo "<br>";
+echo "Luas lingkaran adalah";
+echo "<br>";
+echo "$phi x ($r)²=";
+echo $phi * $r * $r;
+?>
